bd7d64528692b88d42c6b92282b85dc5068b5221
keroti <peterkeroti55@gmail.com>
2023-10-23 23:21:00 +0300

    'Changes'

--- app/main.py
+++ app/main.py
@@ -27,4 +27,4 @@ app.include_router(vote.router)
 
 @app.get("/")
 def root():
-	return {"message": "Hello Keroti! How are you?"}
+	return {"message": "Hello Keroti! How are you?\nThis is fun to learn"}
