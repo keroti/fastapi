7a945a57d5d0255c9d0845636cb0e4f2432a5993
keroti <peterkeroti55@gmail.com>
2023-11-16 23:57:40 +0300

    Build api app

--- app/config.py
+++ app/config.py
@@ -1,4 +1,5 @@
 from pydantic_settings import BaseSettings
+import os
 
 
 class Settings(BaseSettings):
@@ -15,10 +16,10 @@ class Settings(BaseSettings):
 settings = Settings(
     database_hostname="shutterlink.postgres.database.azure.com",
     database_port="5432",
-    database_password="@99kigan",
+    database_password=f"{os.environ.get('database_password')}",
     database_name="fastapi",
     database_username="keroti",
-    secret_key="09d25e094faa6ca2556c818166b7a9563b93f7099f6f0f4caa6cf63b88e8d3e7",
+    secret_key=f"{os.environ.get('secret_key')}",
     algorithm="HS256",
     access_token_expire_minutes=60,
 )
